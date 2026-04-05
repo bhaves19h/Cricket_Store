b8bace3e53d5b1ca02d145fc85093587c27f0287
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2026-04-05 15:11:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   })
 } else {
   app.get("/", (req, res) => {
-    res.send("Cricket Store API Running")
+    res.send("Cricket Store API is Running")
   })
 }
 
